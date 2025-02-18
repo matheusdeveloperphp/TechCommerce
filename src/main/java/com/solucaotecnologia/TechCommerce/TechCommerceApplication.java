5313f4c903a28168d69d1e6d4d3fbbb6d8e6bc1e
matheusdeveloperphp <matheus.solucaotecnologia@gmail.com>
2025-02-17 21:26:42 -0300

    starting e-commerce project

---
+++ src/main/java/com/solucaotecnologia/TechCommerce/TechCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.solucaotecnologia.TechCommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solucaotecnologia/TechCommerce/TechCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solucaotecnologia.TechCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
