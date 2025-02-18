5e1df18b0dbc755ca53f3882f2704725a0f2fdca
matheusdeveloperphp <matheus.solucaotecnologia@gmail.com>
2025-02-18 00:21:08 -0300

    Payment OneToOne Order relationship

--- src/main/java/com/solucaotecnologia/TechCommerce/entities/Order.java
+++ src/main/java/com/solucaotecnologia/TechCommerce/entities/Order.java
@@ -22,14 +22,19 @@ public class Order {
     @JoinColumn(name = "client_id")
     private User client;
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
+
     public Order() {
     }
 
-    public Order(User client, Long id, Instant moment, OrderStatus status) {
-        this.client = client;
+    public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
         this.id = id;
         this.moment = moment;
         this.status = status;
+        this.client = client;
+        this.payment = payment;
     }
 
     public Long getId() {
@@ -64,5 +69,11 @@ public class Order {
         this.client = client;
     }
 
+    public Payment getPayment() {
+        return payment;
+    }
 
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
 }
