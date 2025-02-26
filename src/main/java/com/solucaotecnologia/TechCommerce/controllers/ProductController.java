57fd4ca78794237afcc4b0e64306361ef1c9c583
matheusdeveloperphp <matheus.solucaotecnologia@gmail.com>
2025-02-26 20:59:54 -0300

    GET product Products paged

--- src/main/java/com/solucaotecnologia/TechCommerce/controllers/ProductController.java
+++ src/main/java/com/solucaotecnologia/TechCommerce/controllers/ProductController.java
@@ -5,11 +5,14 @@ import com.solucaotecnologia.TechCommerce.entities.Product;
 import com.solucaotecnologia.TechCommerce.repositories.ProductRepository;
 import com.solucaotecnologia.TechCommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -21,9 +24,13 @@ public class ProductController {
 
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id) {
-        ProductDTO productDTO = productService.findById(id);
-        ProductDTO productDTO1 = productDTO;
-        return productDTO1;
+        return productService.findById(id);
+    }
+
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return productService.findAll(pageable);
     }
 
 }
--- src/main/java/com/solucaotecnologia/TechCommerce/services/ProductService.java
+++ src/main/java/com/solucaotecnologia/TechCommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.solucaotecnologia.TechCommerce.dto.ProductDTO;
 import com.solucaotecnologia.TechCommerce.entities.Product;
 import com.solucaotecnologia.TechCommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -27,4 +30,15 @@ public class ProductService {
             throw new RuntimeException("Produto não encontrado");
         }
     }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> list = productRepository.findAll(pageable);
+        return list.map(x -> new ProductDTO(x));
+    }
+
+
+
+
+
 }
